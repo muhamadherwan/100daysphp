d014ba85bd512aa66678f2dfc5761cf3d01081ce
muhamadherwan <hello.wanlab@gmail.com>
2023-03-18 13:43:20 +0800

    first commit

---
+++ demo/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="">
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+</head>
+<body>
+<h1>
+    <?php
+    echo "Hello there!";
+    ?>
+</h1>
+</body>
+</html>
