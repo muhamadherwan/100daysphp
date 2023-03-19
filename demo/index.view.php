e62c8562a701691215d81826a102568eb56e4b8e
muhamadherwan <hello.wanlab@gmail.com>
2023-03-19 14:39:01 +0800

    view template using require

---
+++ demo/index.view.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="">
+<head>
+    <meta charset="UTF-8">
+    <title>Recommended Books</title>
+</head>
+<style>
+</style>
+<body>
+
+<ul>
+    <?php foreach ($filteredBooks as $book): ?>
+        <li>
+            <a href="<?= $book['url'] ?>">
+                <?= $book['name'] ?>
+            </a>
+            <?= $book['year'] ?>
+            by <?= $book['author'] ?>  
+        </li>
+    <?php endforeach; ?>
+</ul>
+
+</body>
+</html>
